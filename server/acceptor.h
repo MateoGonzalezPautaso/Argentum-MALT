bf6ce09807b8618d1565d5918685ea41d7953065
Mateo Gonzalez Pautaso <mateogonzalezpautaso@gmail.com>
2026-05-19 19:51:15 -0300

    feat: add Acceptor thread

---
+++ server/acceptor.h
@@ -0,0 +1,24 @@
+#ifndef ACCEPTOR_H
+#define ACCEPTOR_H
+
+#include "../common/queue.h"
+#include "../common/socket.h"
+#include "../common/thread.h"
+
+#include "client_list_monitor.h"
+#include "player_command.h"
+
+// Accepts incoming connections in a loop and registers each one in the monitor.
+class Acceptor : public Thread {
+    Socket& listener;
+    Queue<PlayerCommand>& input_queue;
+    ClientListMonitor& monitor;
+
+public:
+    Acceptor(Socket& listener, Queue<PlayerCommand>& input_queue, ClientListMonitor& monitor);
+
+    void run() override;
+    void stop() override;
+};
+
+#endif  // ACCEPTOR_H
