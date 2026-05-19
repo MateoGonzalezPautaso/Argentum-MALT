b29e35b44511a275e8d82b20e2f2ec0b26bb4529
Mateo Gonzalez Pautaso <mateogonzalezpautaso@gmail.com>
2026-05-19 14:32:58 -0300

    feat: add Server Receiver thread

---
+++ server/receiver.cpp
@@ -0,0 +1,17 @@
+#include "receiver.h"
+
+Receiver::Receiver(uint16_t player_id, ServerProtocol& protocol, Queue<PlayerCommand>& input_queue):
+        player_id(player_id), protocol(protocol), input_queue(input_queue) {}
+
+void Receiver::run() {
+    try {
+        while (should_keep_running()) {
+            ClientCommand cmd = protocol.recv_command();
+            input_queue.push(PlayerCommand{player_id, std::move(cmd)});
+        }
+    } catch (const ClosedQueue&) {
+        // Input queue closed (server shutting down).
+    } catch (const std::exception&) {
+        // Connection dropped or recv failed.
+    }
+}
---
+++ server/receiver.h
@@ -0,0 +1,25 @@
+#ifndef RECEIVER_H
+#define RECEIVER_H
+
+#include <cstdint>
+
+#include "../common/messages.h"
+#include "../common/queue.h"
+#include "../common/thread.h"
+
+#include "player_command.h"
+#include "server_protocol.h"
+
+// Blocks on recv_command() and pushes each input_queue command (tagged with
+// player_id) into the shared queue for the game loop
+class Receiver : public Thread {
+    uint16_t player_id;
+    ServerProtocol& protocol;
+    Queue<PlayerCommand>& input_queue;
+
+public:
+    Receiver(uint16_t player_id, ServerProtocol& protocol, Queue<PlayerCommand>& input_queue);
+    void run() override;
+};
+
+#endif  // RECEIVER_H
