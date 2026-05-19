56aaa856a651de3020e31c40a37db8be000fef00
Mateo Gonzalez Pautaso <mateogonzalezpautaso@gmail.com>
2026-05-19 12:02:43 -0300

    feat: add PlayerCommand for game purposes

---
+++ server/player_command.h
@@ -0,0 +1,14 @@
+#ifndef PLAYER_COMMAND_H
+#define PLAYER_COMMAND_H
+
+#include <cstdint>
+
+#include "../common/messages.h"
+
+// A ClientCommand tagged with the player_id that sent it
+struct PlayerCommand {
+    uint16_t player_id;
+    ClientCommand cmd;
+};
+
+#endif  // PLAYER_COMMAND_H
